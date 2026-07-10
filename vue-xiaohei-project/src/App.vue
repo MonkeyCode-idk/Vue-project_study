0ec65212794e405c3deac3bd6e1d4e344a8add82
puppy <3285093291@qq.com>
2026-07-11 02:54:16 +0800

    小黑记事本组件版添加功能 第二次提交

--- vue-xiaohei-project/src/App.vue
+++ vue-xiaohei-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 主体区域 -->
   <section id="app">
-   <TodoHeader></TodoHeader>
+   <TodoHeader @add="hhandleAdd"></TodoHeader>
    <TodoMain :list="list"></TodoMain>
    <TodoFooter></TodoFooter> 
   
@@ -16,6 +16,13 @@ import TodoFooter from './components/TodoFooter.vue';
 // 渲染功能:
 // 1.提供数据 -> 提供在公共的父组件 App.vue
 // 2.通过父传子，将数据传递给TodoMain
+// 3.利用 v-for 渲染
+
+// 添加功能：
+// 1.收集表单数据 -> v-model
+// 2.监听事件（回车 + 点击 都要进行添加）
+// 3.子传父，将任务名称传递给父组件App.vue
+// 4.进行添加 unshift(自己的数据自己负责)
 export default {
   data() {
     return {
@@ -24,6 +31,15 @@ export default {
         {id:2,name:'逛街'},
         {id:3,name:'玩游戏'},
       ]
+    }
+  },
+  methods: {
+    hhandleAdd(todoName) {
+      console.log(todoName);
+      this.list.unshift({
+        id: +new Date(),
+        name:todoName
+      })
     }
   },
   components: {
--- vue-xiaohei-project/src/components/TodoHeader.vue
+++ vue-xiaohei-project/src/components/TodoHeader.vue
@@ -1,12 +1,30 @@
 <template>
      <header class="header">
       <h1>小黑记事本</h1>
-      <input placeholder="请输入任务" class="new-todo" />
-      <button class="add">添加任务</button>
+      <input 
+      @keyup.enter="handleAdd"
+      v-model="todoName" placeholder="请输入任务" class="new-todo" />
+      <button class="add" @click="handleAdd">添加任务</button>
     </header>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      todoName:''
+    }
+  },
+  methods: {
+    handleAdd() {
+      // console.log(this.todoName);
+      if(this.todoName.trim() === '') return alert('输入不能为空')
+      this.$emit('add',this.todoName)
+      this.todoName = ''
+      
+    }
+  }
+}
 </script>
 
 <style>
