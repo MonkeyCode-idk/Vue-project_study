a03dcb206f3a03faf1e7e114a3483d6ac64f021c
puppy <3285093291@qq.com>
2026-07-19 18:38:36 +0800

    vue-sync修饰符 第一次提交

---
+++ vue-sync/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <button class="logout"
+    @click="isShow = true"
+    >退出按钮</button>
+
+    <!-- :visible.sync => :visible + @update:visible-->
+   <BaseDialog
+   :visible.sync="isShow"
+   ></BaseDialog>
+  </div>
+</template>
+
+<script>
+import BaseDialog from './components/BaseDialog.vue';
+export default {
+  data() {
+    return {
+    isShow:false
+  }
+ },
+  components: {
+  BaseDialog
+  }
+}
+</script>
+
+<style>
+
+</style>
