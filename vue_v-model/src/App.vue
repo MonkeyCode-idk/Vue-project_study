3a8a4e55f19b08cc7681fa20e875cbe7c5048750
puppy <3285093291@qq.com>
2026-07-19 17:58:07 +0800

    v-model介绍

---
+++ vue_v-model/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <BaseSelect
+   :cityId="selectId"
+   @changeId="selectId = $event"
+   ></BaseSelect>
+<p>----------------</p>
+   <p>v-model传值,子 Input触发</p>
+   <!-- v-model => :value + @input -->
+   <BaseSelectB 
+   v-model="selectId"
+   ></BaseSelectB>
+  </div>
+</template>
+
+<script>
+import BaseSelect from './components/BaseSelect.vue';
+
+import BaseSelectB from './components/BaseSelectB.vue';
+export default {
+  data() {
+    return {
+      selectId: '104'
+    }
+ },
+  components: {
+      BaseSelect,
+      BaseSelectB,
+  }
+}
+</script>
+
+<style>
+
+</style>
